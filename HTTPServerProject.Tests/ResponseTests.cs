b58f9ae0975988465aab66e663bf6c63181c962d
evan-m-jackson <90336648+evan-m-jackson@users.noreply.github.com>
2022-11-21 16:59:22 -0500

    Light Refactor of Code
    - Delete 'HTTPServer.service' file
    - Refactor 'Tar up the build step' in azure-vm.yml file
    - Remove excess whitespace in IntegrationTest.cs file

--- HTTPServerProject.Tests/ResponseTests.cs
+++ HTTPServerProject.Tests/ResponseTests.cs
@@ -22,7 +22,7 @@ public class UnitTestsForResponses
     {
         var expected = new List<string>() { "HTTP/1.1 200 OK", "Location: Earth", "", "Hello World!" };
         var writer = new TestWriteStreams(eStream);
-        var headers = new List<string>(){"Location: Earth"};
+        var headers = new List<string>() { "Location: Earth" };
         var body = "Hello World!";
 
         var response = new WriteResponse(writer, 200, body, headers);
