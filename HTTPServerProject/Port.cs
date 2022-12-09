4df000240a4b1b3dc46f71e2ecf447ff2bfc43ab
evan-m-jackson <90336648+evan-m-jackson@users.noreply.github.com>
2022-12-09 14:51:21 -0500

    Refactor Port class
    - Create IsPortValid method to return a boolean if the port number is between the range of 0 to 65535.
    - Create GetPortNum method to returns the port string as an int or 5000 as default

--- HTTPServerProject/Port.cs
+++ HTTPServerProject/Port.cs
@@ -6,6 +6,31 @@ public class Port
     
     public int GetPort(string[] args)
     {
+        var port = GetPortNum(args);
+        if (IsPortValid(port))
+        {
+            return port;
+        }
+        else
+        {
+            return 5000;
+        }
+    }
+
+    public bool IsPortValid(int port)
+    {
+        if (port >= 0 && port <= 65535)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+    public int GetPortNum(string[] args)
+    {
         try
         {
             var portStr = args[0];
